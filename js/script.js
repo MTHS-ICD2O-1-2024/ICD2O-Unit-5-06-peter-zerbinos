55625773abd6150d8b022f4fa1124527a9d89205
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-05 02:09:35 +0000

    added changes to index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function determines the type of triangle.
+ */
+function multiplyThroughAddition() {
+  const positiveInteger = parseInt(document.getElementById("positive-integer").value);
+  const counter = parseInt(document.getElementById("counter").value);
+  const product = 0;
+
+  while (counter > 0) {
+    product = product + positiveInteger
+  }
+}
